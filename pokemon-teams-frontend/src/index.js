54fa7fa42089f512ff2424c260ee3b60b116bfec
sonyavera <sonyavgould@gmail.com>
2020-08-10 16:50:30 -0400

    Working solution frontend

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -5,108 +5,79 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`
 let trainersData = {};
 const main = document.querySelector("main")
 
-// document.addEventListener("DOMContentLoaded", function(e) {
 
-    const getTrainers = () => {
-        fetch(TRAINERS_URL)
-        .then(response => response.json())
-        .then(responseData => {
-            trainersData = responseData
-            responseData.forEach(renderTrainer)
-            // console.log(trainersData)
-        })
-    }
-    
+const getTrainers = () => {
+    fetch(TRAINERS_URL)
+    .then(response => response.json())
+    .then(responseData => {
+        trainersData = responseData
+        responseData.forEach(renderTrainer)
+    })
+}
 
+const releasePokemon = (e) => {
+    const pokemonLi = e.target.parentElement
+    const pokemonId = pokemonLi.dataset.pokemonId
+    fetch(POKEMONS_URL + "/" + pokemonId, {method: "DELETE"})
+    .then(resp => {pokemonLi.remove()})
+}
 
+const renderNewPokemon = (pokemonObj, trainerCard) => {
+    console.log(pokemonObj)
+    const pokemonUl = trainerCard.lastChild
+    const pokemonLi = document.createElement("li")
+    pokemonLi.dataset.pokemonId = pokemonObj.id
+    pokemonLi.innerHTML = `${pokemonObj.nickname}<button class="release">Release</button>`
+    pokemonLi.addEventListener("click", releasePokemon)
+    pokemonUl.append(pokemonLi)
+}
 
-    const releasePokemon = (e) => {
-        const pokemonLi = e.target.parentElement
-        const pokemonId = pokemonLi.dataset.pokemonId
-        // const options = {
-        //     method: “DELETE”
-        //   }
-        fetch(POKEMONS_URL + "/" + pokemonId, {method: "DELETE"})
-        .then(res => {pokemonLi.remove()})
-    }
-
-    const renderNewPokemon = (pokemonObj) => {
-        console.log(pokemonObj.trainerId)
-    }
-
-
-    const addPokemon = (e) => {
-
-        const trainerId = e.target.dataset.trainerId
-        console.log(trainerId)
-
-        const options = {
-            method: "POST",
-            headers: {
-              "content-type": "application/json", 
-              "accept": "application/json"
-            },
-            body: JSON.stringify("trainer_id": trainerId)
-          }
-          fetch(POKEMONS_URL, options) 
-          .then(response => renderNewPokemon(response))
+const addPokemon = (e) => {
+    const trainerId = e.target.dataset.trainerId
+    const trainerCard = e.target.parentElement
+    const options = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({"trainer_id": trainerId})
         }
-
-
-    const renderTrainer = (trainerObj) => {
-        const trainerCard = document.createElement('div')
-        trainerCard.classList.add('card')
-        trainerCard.dataset.id = trainerObj.id 
-        trainerCard.innerHTML = `
-        <p>${trainerObj.name}</p>
-        `
-        
-        const addPokemonButton = document.createElement("button")
-        addPokemonButton.textContent = `Add Pokemon`
-        addPokemonButton.dataset.trainerId = trainerObj.id
-
-        addPokemonButton.addEventListener('click', addPokemon)
-
-        trainerCard.append(addPokemonButton)
-
-        const pokemonUl = document.createElement("ul")
-        trainerCard.append(pokemonUl)
-
-        trainerObj.pokemons.forEach(function(pokemon){
-            const pokemonLi = document.createElement("li")
-            pokemonLi.dataset.pokemonId = pokemon.id
-            pokemonLi.innerHTML = `
-            ${pokemon.nickname}<button class="release">Release</button>
-            `
-
-            
-
-            pokemonLi.addEventListener("click", releasePokemon)
-
-            pokemonUl.append(pokemonLi)
-        })
-
-
-
-
-
-
-
-        main.append(trainerCard)
-
+        fetch(POKEMONS_URL, options) 
+        .then(resp => resp.json())
+        .then(response => renderNewPokemon(response, trainerCard))
     }
 
 
+const renderTrainer = (trainerObj) => {
+    const trainerCard = document.createElement('div')
+    trainerCard.classList.add('card')
+    trainerCard.dataset.id = trainerObj.id 
+    trainerCard.innerHTML = `<p>${trainerObj.name}</p>`
+
+    const addPokemonButton = document.createElement("button")
+    addPokemonButton.textContent = `Add Pokemon`
+    addPokemonButton.dataset.trainerId = trainerObj.id
+    addPokemonButton.addEventListener('click', addPokemon)
+    trainerCard.append(addPokemonButton)
+
+    const pokemonUl = document.createElement("ul")
+    trainerCard.append(pokemonUl)
+
+    trainerObj.pokemons.forEach(function(pokemon){
+        const pokemonLi = document.createElement("li")
+        pokemonLi.dataset.pokemonId = pokemon.id
+        pokemonLi.innerHTML = `
+        ${pokemon.nickname}<button class="release">Release</button>
+        `
+        pokemonLi.addEventListener("click", releasePokemon)
+        pokemonUl.append(pokemonLi)
+    })
+
+    main.append(trainerCard)
+}
+
+
     
-    getTrainers()
-// })
+getTrainers()
 
 
-// <button data-trainer-id="1">Add Pokemon</button>
-// <ul>
-//     <li>Jacey (Kakuna) <button class="release" data-pokemon-id="140">Release</button></li>
-//     <li>Zachariah (Ditto) <button class="release" data-pokemon-id="141">Release</button></li>
-//     <li>Mittie (Farfetch'd) <button class="release" data-pokemon-id="149">Release</button></li>
-//     <li>Rosetta (Eevee) <button class="release" data-pokemon-id="150">Release</button></li>
-//     <li>Rod (Beedrill) <button class="release" data-pokemon-id="151">Release</button></li>
-// </ul>
